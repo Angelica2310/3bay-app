df42d5061ffbdf229a823a38ba8187653f846538
LizNich <nicholsonliz@hotmail.com>
2024-12-13 15:56:59 +0000

    profile form and tailwind

--- src/components/BuyerProfile.jsx
+++ src/components/BuyerProfile.jsx
@@ -1,14 +1,34 @@
 // This is the BUYER profile page
 // links to Search Page - CHANGE TO CORRECT PATH BELOW
 
+import Link from "next/link";
+import { db } from "@/utils/db";
+import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
-export default function BuyerProfile() {
-  //revalidatePath("/");
+export default async function BuyerProfile() {
+  const { userId } = await auth();
+
+  async function handleSubmit(formData) {
+    "use server";
+    const username = formData.get("username");
+    const email = formData.get("email");
+    //console.log(username);
+
+    db.query(
+      `INSERT INTO users (username, email, clerk_id) VALUES ($1, $2, $3)`,
+      [username, email, userId]
+    );
+
+    revalidatePath("/");
+  }
 
   return (
     <div className="bg-gray-100 flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center space-y-6 bg-white p-8 rounded-lg shadow-lg ">
+      <div
+        className="flex flex-col items-center space-y-6 bg-white p-8 max-w-2xl rounded-md shadow-lg
+    "
+      >
         <img
           src="https://via.placeholder.com/150"
           alt="Profile pic"
@@ -17,16 +37,22 @@ export default function BuyerProfile() {
         <input
           type="user_name"
           placeholder="user name"
-          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="input-style"
         ></input>
 
         <input
           type="email"
-          placeholder="email from clerk_id?"
-          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="email address"
+          className="input-style"
         ></input>
-        <button className="button-style">Submit</button>
-        <button className="button-style">Become a Seller</button>
+
+        <button className="submit-style">Submit</button>
+
+        <div>
+          <Link href="/">
+            <button className="button-style">Become a Seller</button>
+          </Link>
+        </div>
 
         <div className="w-full bg-gray-200 p-4 rounded-lg mt-6">
           <h2 className="text-lg font-semibold text-gray-800">
@@ -36,21 +62,6 @@ export default function BuyerProfile() {
             <img
               src="https://via.placeholder.com/150"
               alt="Product 1"
-              className="w-full h-auto border-2 border-gray-300"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Product 2"
-              className="w-full h-auto border-2 border-gray-300"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Product 3"
-              className="w-full h-auto border-2 border-gray-300"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Product 4"
               className="w-full h-auto border-2 border-gray-300"
             />
           </div>
