d6ad26254c20e9df07055c979f39b643c55cb02e
jamie <level7jamie@gmail.com>
2024-12-13 11:37:36 +0000

    Function to register users in database

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,17 +4,21 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/utils/db";
 import BuyerProfile from "@/components/BuyerProfile";
 import SellerProfile from "@/components/SellerProfile";
+import { CheckForUser } from "@/utils/actions";
 
 export default async function ProfilePage() {
-  const { userId } = await auth();
+  // const { userId } = await auth();
+
+  // Checks if user exists in db and add to db if not. Returns user obj with id, username, email, image
+  CheckForUser();
 
   // Check if the user exists in the database and if they are a seller
   // const response = await db.query(
-  //   `SELECT * users, shops.id AS shop_id
-  //    FROM users
-  //    LEFT JOIN shops ON users.id = shops.user_id
-  //    WHERE users.clerk_id = $1`,
-  //   [userId]
+  // `SELECT * users, shops.id AS shop_id
+  //  FROM users
+  //  LEFT JOIN shops ON users.id = shops.user_id
+  //  WHERE users.clerk_id = $1`,
+  // [userId]
   // );
   // console.log(response);
 
