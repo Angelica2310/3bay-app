0150c2a592e08ac1575110f9aa0de077a0e9b6fc
Angelicale <gianghgle@gmail.com>
2024-12-15 01:03:10 +0000

    feature products created

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,5 +1,59 @@
+import { db } from "@/Utils/pg";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+Link;
 
-export default function AllProductPage() {
-  return <div>All products</div>;
+export default async function FeatureProducts() {
+  const products = (
+    await db.query(`SELECT 
+    products.name,
+    products.description,
+    products.price,
+    images.url AS images_url
+FROM 
+    products
+LEFT JOIN 
+    images
+ON 
+    products.id = images.products_id;`)
+  ).rows;
+
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <h1 className="mt-12 text-2xl">All products</h1>
+      <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap ">
+        {products.map((product) => {
+          return (
+            <div key={products.id}>
+              <Link
+                href={`/products/${product.id}`}
+                className="w-full flex flex-col gap-4"
+              >
+                <div className="relative w-60 h-60">
+                  <Image
+                    src={product.images_url}
+                    alt={product.name}
+                    layout="fill"
+                    objectFit="cover"
+                    sizes="25vw"
+                    className="absolute rounded-md"
+                  />
+                </div>
+
+                <div className="flex justify-between">
+                  <span className="font-medium">{product.name}</span>
+                  <span className="font-semibold">£{product.price}</span>
+                </div>
+                <div className="text-sm text-gray-500">
+                  {product.description}{" "}
+                </div>
+                <button className="cart-button">Add to Cart</button>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
