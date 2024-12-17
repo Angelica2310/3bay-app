6ee3ecd9b9a8a46a927a64a26fb6d65769fca139
Bertie Raffle <bertierwork@gmail.com>
2024-12-17 14:28:24 +0000

    add to cart button

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Modelviewer from "./ModelViewer";
+import AddToCartButton from "./AddToCartButton";
 
 export default async function Product({ product, ownShop }) {
   const sideLng = 240;
@@ -30,7 +31,7 @@ export default async function Product({ product, ownShop }) {
             className="rounded-md"
           />
         ) : (
-          <div className="relative w-60 h-60">
+          <div className="relative w-56 h-56">
             <Image
               src={altImg.url}
               alt={altImg.id}
@@ -46,29 +47,27 @@ export default async function Product({ product, ownShop }) {
   }
 
   return (
-    <Link
-      href={`/products/${product.id}`}
-      key={product.id}
-      className="bg-white rounded-lg w-60 h-80"
-    >
-      <div id="imageModel">
-        {/*modelData.rowCount !== 0 ? (
+    <div key={product.id} className="bg-white rounded-lg w-60 h-80 p-2">
+      <Link href={`/products/${product.id}`}>
+        <div id="imageModel">
+          {/*modelData.rowCount !== 0 ? (
           <Modelviewer id={modelData.id} className="w-60 h-60" />
         ) : (*/}
-        <div children={altImage()}></div>
-      </div>
-      <div className="flex justify-between">
-        <span className="font-medium">{product.name}</span>
-        <span className="font-semibold">£{product.price}</span>
-      </div>
-      <div className="text-sm text-gray-500">{product.description}</div>
-      {/* If ownShop bool was passed as prop and is true (product was added by logged in user) then show edit product button, otherwise add to cart button */}
+          <div children={altImage()}></div>
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">{product.name}</span>
+          <span className="font-semibold">£{product.price}</span>
+        </div>
+        <div className="text-sm text-gray-500">{product.description}</div>
+        {/* If ownShop bool was passed as prop and is true (product was added by logged in user) then show edit product button, otherwise add to cart button */}
+      </Link>
       {ownShop ? (
         <button className="cart-button">Edit Product</button>
       ) : (
-        <button className="cart-button">Add to Cart</button>
+        <AddToCartButton className="cart-button" product={product} />
       )}
       {/* <button className="cart-button">Add to Cart</button> */}
-    </Link>
+    </div>
   );
 }
