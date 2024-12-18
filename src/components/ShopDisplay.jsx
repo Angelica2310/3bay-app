91f66e4e36119e7d9a0428041ae941831d27fb82
LizNich <nicholsonliz@hotmail.com>
2024-12-18 21:03:11 +0000

    added shop themes

--- src/components/ShopDisplay.jsx
+++ src/components/ShopDisplay.jsx
@@ -9,21 +9,28 @@ export default async function ShopDisplay({ shop, ownShop }) {
   const products = await GetShopProducts(shop.id);
 
   return (
-    <div className="flex flex-col h-full w-full mx-6 items-center">
-      <h3 className="mt-10 bg-gingeralefizz px-6 py-2 rounded-t-3xl text-2xl pb-1 font-bold">
+    <div
+      className={`bg-color-${shop.theme} bg-color-card-${shop.theme} flex flex-col h-full w-full  items-center `}
+    >
+      <h3
+        className={`bg-color-card-${shop.theme} text-color-text-${shop.theme} mt-10 px-6 py-2 rounded-t-3xl text-2xl pb-1 font-bold`}
+      >
         My Shop
       </h3>
-      <div className="mb-10 flex flex-col items-center w-4/5 bg-gingeralefizz py-4 px-4 rounded-3xl">
+      <div
+        className={`bg-color-card-${shop.theme} text-color-text-${shop.theme} mb-10 flex flex-col items-center w-4/5 py-4 px-4 rounded-3xl`}
+      >
         <h2 className="mt-2 text-3xl sm:text-4xl font-semibold text-center">
           {shop.name}
         </h2>
+
         <p className="py-4 text-xl text-justify">{shop.description}</p>
         <div className="flex flex-col sm:flex-row mt-2 mb-4">
           {ownShop && <AddProductBtn shopId={shop.id} />}
           {ownShop && (
             <Link
               href="/store/edit"
-              className="py-1 px-3 mt-6 sm:mt-0 sm:ml-10 text-lg rounded-xl bg-prismarine font-semibold text-white shadow-[0_2px_10px] shadow-black hover:bg-benihired hover:text-white focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none text-center"
+              className="py-1 px-3 mt-6 sm:mt-0 sm:ml-10 text-lg rounded-xl bg-prismarine  text-white shadow-[0_2px_10px] shadow-black hover:bg-benihired hover:text-white focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none text-center pl-5 pr-5"
             >
               Edit Shop
             </Link>
