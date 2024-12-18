faf2036d5b68998b5c0e9ac7e1c06d016fc0dabe
Angelicale <gianghgle@gmail.com>
2024-12-18 21:09:53 +0000

    change payment payment

--- src/app/payment-success/page.js
+++ src/app/payment-success/page.js
@@ -4,10 +4,10 @@ export default async function PaymentSuccess({ searchParams }) {
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-bratwurst">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-        <h2 className="text-2xl">You successfully sent</h2>
+        <h2 className="text-2xl">You have successfully sent</h2>
 
         <div className="bg-white p-2 rounded-md text-stormygreen mt-5 text-4xl font-bold">
-          £{totalAmount}
+          £{(totalAmount || 0).toFixed(2)}
         </div>
       </div>
     </main>
